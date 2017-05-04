47e019cf798fac1ac6719f59ec34994dd65d3285
skt2608 <sachin.tiwari@stonybrook.edu>
2017-05-04 19:19:58 -0400

    fixed semantic bug in URI

--- restServer.py
+++ restServer.py
@@ -10,7 +10,7 @@ def index():
 def getBreakDown():
     return jsonify(minedData[0])
 
-@app.route("/league/scores/<int:leagueId>",methods=['GET'])
+@app.route("/league/<int:leagueId>/scores",methods=['GET'])
 def getLeagueScore(leagueId):
     leagues = utils.getAllDatafromTable('league')
     leagueName=''
@@ -18,7 +18,7 @@ def getLeagueScore(leagueId):
         if league[0] == leagueId:leagueName = league[2]
     return jsonify(minedData[1].get(leagueName))
 
-@app.route("/league/transfers/<int:leagueId>",methods=['GET'])
+@app.route("/league/<int:leagueId>/transfers",methods=['GET'])
 def getLeagueTransfer(leagueId):
     leagues = utils.getAllDatafromTable('league')
     leagueName=''
@@ -48,8 +48,8 @@ def getLeagueTeams(leagueId):
     return jsonify(minedLeagueData[1].get(leagueName))
 if __name__ == '__main__':
     # first get all the data , before opening the port for client access.
-    #minedData = utils.temp1()
-    #minedTransfers = utils.temp2()
+    minedData = utils.temp1()
+    minedTransfers = utils.temp2()
     minedLeagueData = utils.topPlayerEvolution()
     app.run()
     
